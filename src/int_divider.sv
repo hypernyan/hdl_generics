11f1d3165316dcf1a3b8f254d43484f30ecab2ee
Arseniy <hypernyan@gmail.com>
2020-09-08 18:43:45 +0300

    Add 'recalculate' input

--- src/int_divider.sv
+++ src/int_divider.sv
@@ -6,22 +6,24 @@ module int_divider #(
 (
   input logic clk,
   input logic rst,
+  input logic cal,
   input logic [WIDTH-1:0] dvd, // divident
   input logic [WIDTH-1:0] dvs, // divisor
   output logic [WIDTH-1:0] quo,
   output logic rdy
 );
 
-logic [WIDTH-1:0] dvd_prev;
-logic [WIDTH-1:0] dvs_prev;
 logic [WIDTH-1:0] cmp;
 logic [WIDTH-1:0] dvd_reg;
 logic [$clog2(WIDTH+1)-1:0] cnt;
+logic rdy_reg;
 
 enum logic [2:0] {
   idle_s,
   calc_s
 } fsm;
+
+assign rdy = rdy_reg && !cal;
   
 always @ (posedge clk) begin
   if (rst) begin
@@ -29,7 +31,7 @@ always @ (posedge clk) begin
     dvd_prev <= 0;
     dvd_reg  <= 0;
     dvs_prev <= 0;
-    rdy <= 0;
+    rdy_reg <= 0;
     cmp <= 0;
     quo <= 0;
   end
@@ -39,9 +41,9 @@ always @ (posedge clk) begin
         cnt <= 0;
         dvd_prev <= dvd;
         dvs_prev <= dvs;
-        if (dvd != dvd_prev || dvs != dvs_prev) begin
+        if (cal) begin
           $display("Divident: %d (%b). Divisor: %d (%b).", dvd, dvd, dvs, dvs);
-          rdy <= 0;
+          rdy_reg <= 0;
           quo <= 0;
           cmp <= 0;
           dvd_reg <= dvd;
@@ -57,7 +59,7 @@ always @ (posedge clk) begin
         cmp[WIDTH-1:0] <= (cmp >= dvs) ? ({cmp[WIDTH-2:0] - dvs, dvd_reg[WIDTH-1]} ) : {cmp[WIDTH-2:0], dvd_reg[WIDTH-1]};
         cnt <= cnt + 1;
         if (cnt == WIDTH) begin
-          rdy <= 1;
+          rdy_reg <= 1;
           fsm <= idle_s;
           $display ("Result: %b. %d", quo, quo);
         end
